128b77b86c11ebd5843cb4030fd7dd1a25b73d33
Timo Feuerstein <timo.feuerstein@rheinpfalz.de>
2019-03-24 11:06:41 +0100

    [TASK] Modify icon handling

--- ext_tables.php
+++ ext_tables.php
@@ -4,11 +4,20 @@ defined('TYPO3_MODE') || die('Access denied.');
 call_user_func(
     function()
     {
-        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-        $iconRegistry->registerIcon(
-            'id4me',
-            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-            ['source' => 'EXT:id4me/Resources/Public/Icons/Extension.png']
-        );
+
+        if (TYPO3_MODE === 'BE') {
+            $icons = [
+                'ext-id4me-extension' => 'Extension.png',
+            ];
+
+            $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+            foreach ($icons as $identifier => $path) {
+                $iconRegistry->registerIcon(
+                    $identifier,
+                    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+                    ['source' => 'EXT:news/Resources/Public/Icons/' . $path]
+                );
+            }
+        }
     }
 );
