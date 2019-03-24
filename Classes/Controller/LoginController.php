8dc8382bd024e6fcdcff85caff108887f198cf0d
Timo Feuerstein <timo.feuerstein@rheinpfalz.de>
2019-03-24 11:11:21 +0100

    [!WiP] Try to fix error in controller

--- Classes/Controller/LoginController.php
+++ Classes/Controller/LoginController.php
@@ -3,10 +3,15 @@
 namespace TmFrstn\Id4me\Controller;
 
 use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class LoginController extends ActionController implements LoggerAwareInterface
 {
+
+    use LoggerAwareTrait;
+
+
     public function formAction()
     {
 
