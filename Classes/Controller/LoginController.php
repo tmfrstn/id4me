30f2e9c184e57088d8af814e4c18780e3f706a40
Timo Feuerstein <timo.feuerstein@rheinpfalz.de>
2019-03-24 17:56:56 +0100

    [!WiP] Improve id4me example

--- Classes/Controller/LoginController.php
+++ Classes/Controller/LoginController.php
@@ -26,7 +26,8 @@ class LoginController extends ActionController implements LoggerAwareInterface
      */
     protected $uriBuilder = NULL;
 
-    protected $identifier = 'idtest1.domainid.community';
+#    protected $identifier = 'idtest1.domainid.community';
+    protected $identifier = 'eisenmann.io';
 
     public function initializeAction()
     {
@@ -35,6 +36,10 @@ class LoginController extends ActionController implements LoggerAwareInterface
 
     public function formAction()
     {
+        if(!isset($_GET['autheticate']) || $_GET['autheticate'] != 'yes') {
+            return $this->view->render();
+        }
+
         $openIdConfig = $this->provideOpenIdConfig();
 
         $client = $this->id4Me->register(
@@ -43,21 +48,24 @@ class LoginController extends ActionController implements LoggerAwareInterface
             sprintf('https://id4me.tmfrstn.de/login/', $this->identifier)
         );
 
-        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($client);
+        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($client,'Client');
+
+        $uniqId = uniqid();
 
         $authorizationUrl = $this->id4Me->getAuthorizationUrl(
             $openIdConfig,
             $client->getClientId(),
             $this->identifier,
             $client->getActiveRedirectUri(),
-            'https://id4me.tmfrstn.de/'
+            $uniqId
         );
 
         $GLOBALS['TSFE']->fe_user->setKey("ses","clientId",$client->getClientId());
         $GLOBALS['TSFE']->fe_user->setKey("ses","clientSecret",$client->getClientSecret());
+        $GLOBALS['TSFE']->fe_user->setKey("ses","uniqId",$uniqId);
         $GLOBALS['TSFE']->storeSessionData();
 
-        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($authorizationUrl);
+        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($authorizationUrl,'authorizationUrl');
 
         $this->redirectToURI($authorizationUrl);
 
@@ -66,31 +74,39 @@ class LoginController extends ActionController implements LoggerAwareInterface
     public function authenticateAction()
     {
 
+        $code = $_GET['code'];
         $clientId = $GLOBALS['TSFE']->fe_user->getKey('ses', 'clientId');
         $clientSecret = $GLOBALS['TSFE']->fe_user->getKey('ses', 'clientSecret');
+        $uniqId = $GLOBALS['TSFE']->fe_user->getKey('ses', 'uniqId');
 
-        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($clientId);
-        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($clientSecret);
+        if($uniqId != $_GET['state']){
+            \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump('NO');
+        } else {
+            \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump('YES');
+        }
+        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($clientId, 'clientId');
+        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($clientSecret, 'clientSecret');
+        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($code, 'code');
 
         $openIdConfig = $this->provideOpenIdConfig();
 
         $accessTokens = $this->id4Me->getAccessTokens(
             $openIdConfig,
-            'WU9ZOZGC0DgzS9blLECd',
-            sprintf('https://id4me.tmfrstn.de/', $this->identifier),
+            $code,
+            sprintf('https://id4me.tmfrstn.de/login/', $this->identifier),
             $clientId,
             $clientSecret
         );
-        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($accessTokens);
+        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($accessTokens, 'accessTokens');
     }
 
     private function provideOpenIdConfig()
     {
         $authorityName = $this->id4Me->discover($this->identifier);
-        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($authorityName);
+        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($authorityName, 'authorityName');
 
         $openIdConfig = $this->id4Me->getOpenIdConfig($authorityName);
-        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($openIdConfig);
+        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($openIdConfig, 'openIdConfig');
 
         return $openIdConfig;
     }
